55474c44a721e4d8f8216e4f1f03cb9bec3e1a5a
Jorge Travieso <jtrav029@fiu.edu>
2015-04-22 15:36:34 -0400

    works but not sure if the algorithm follows the specifications

--- cannon/cannon.c
+++ cannon/cannon.c
@@ -56,43 +56,6 @@ void my_abort(const char* fmt, ...)
 }
  
 
-void printMatrix(datatype * matrix, int x , int y )
-{
-    int i,j;
-    // Moves Row
-    for( i = 0; i < x ; i++ )
-    {
-        for( j = 0; j < y; j++ )
-        {
-            printf("%lf ", matrix[i*x+j] );
-        }
-        printf("\n");
-    }
-}
-
-
-
-void writeFile( datatype *matrix, int x1, int y1)
-{
-    FILE *output = fopen("C.txt", "w");
-    int x,y;
-
-    // Write out the matrix in the ouput file
-    for( x = 0; x < x1; x++ )
-    {
-        for( y = 0; y < y1-1; y++ )
-        {
-//            fprintf(output,"%lf ", matrix[x*y1 + y]);
-            fprintf(output,"%d ", (int)rint(matrix[x*y1 + y]));
-        }
-        // This will prevent the extraspace and instead let's put a newline
-//        fprintf(output,"%lf\n",(int)matrix[x*y1 + y]);
-        fprintf(output,"%d\n",(int)rint(matrix[x*y1 + y]));
-    }
-
-    //Clean up
-    fclose(output);
-}
 
 
 
@@ -108,111 +71,6 @@ void MatrixMultiply(int n, datatype *a, datatype *b, datatype *c)
                 c[i*n+j] += a[i*n+k]*b[k*n+j]; 
 }
 
-
-
-void CannonAlgorithm(int n, datatype *a, datatype *b, datatype *c, MPI_Comm comm) 
-{ 
-      int i; 
-      int nlocal; 
-      int npes, dims[2], periods[2]; 
-      int myrank, my2drank, mycoords[2]; 
-      int uprank, downrank, leftrank, rightrank, coords[2]; 
-      int shiftsource, shiftdest; 
-      MPI_Status status; 
-      MPI_Comm comm_2d; 
-
-      // Print Matrix
-//      if( gRank == 0 )
-//      {
-//          printMatrix(a,n,n);
-//          printf("\n");
-//          printMatrix(b,n,n);
-//      }
-   
-      /* Get the communicator related information */ 
-      MPI_Comm_size(comm, &npes); 
-      MPI_Comm_rank(comm, &myrank); 
-   
-      /* Set up the Cartesian topology */ 
-      dims[0] = dims[1] = sqrt(npes); 
-   
-      /* Set the periods for wraparound connections */ 
-      periods[0] = periods[1] = 1; 
-   
-      /* Create the Cartesian topology, with rank reordering */ 
-      MPI_Cart_create(comm, 2, dims, periods, 1, &comm_2d); 
-   
-      /* Get the rank and coordinates with respect to the new topology */ 
-      MPI_Comm_rank(comm_2d, &my2drank); 
-      MPI_Cart_coords(comm_2d, my2drank, 2, mycoords); 
-   
-      // There needs to be some switching done here
-      /* Compute ranks of the up and left shifts */ 
-      MPI_Cart_shift(comm_2d, 1, -1, &rightrank, &leftrank); 
-      MPI_Cart_shift(comm_2d, 0, -1, &downrank, &uprank); 
-   
-      /* Determine the dimension of the local matrix block */ 
-      nlocal = n/dims[0]; 
-   
-      /* Perform the initial matrix alignment. First for A and then for B */ 
-//      MPI_Cart_shift(comm_2d, 0, -mycoords[0], &shiftsource, &shiftdest); 
-      MPI_Cart_shift(comm_2d, 1, -mycoords[0], &shiftsource, &shiftdest); 
-
-      MPI_Sendrecv_replace(a, nlocal*nlocal, MPI_DOUBLE, shiftdest, 
-          1, shiftsource, 1, comm_2d, &status); 
-   
-//      MPI_Cart_shift(comm_2d, 1, -mycoords[1], &shiftsource, &shiftdest); 
-      MPI_Cart_shift(comm_2d, 0, -mycoords[1], &shiftsource, &shiftdest); 
-      MPI_Sendrecv_replace(b, nlocal*nlocal, MPI_DOUBLE, 
-          shiftdest, 1, shiftsource, 1, comm_2d, &status); 
-   
-      /* Get into the main computation loop */ 
-    for (i=0; i<dims[0]; i++) 
-    { 
-//        if( gRank == 0 )
-//        {
-//            printMatrix(b,n,n);
-//            puts("");
-//        }
-//        //Print C before
-//        puts("Before: C Matrix");
-//        printMatrix(c,n,n);
-        MatrixMultiply(nlocal, a, b, c); /*c=c+a*b*/ 
-
-
-//        //Print C after
-//        puts("After: C Matrix");
-//        printMatrix(c,n,n);
-
-        /* Shift matrix a left by one */ 
-        MPI_Sendrecv_replace(a, nlocal*nlocal, MPI_DOUBLE, 
-        leftrank, 1, rightrank, 1, comm_2d, &status); 
-//        printf("Proc1: %d\nProc2: %d Dims:%d\n",my2drank,gRank,i); 
-
-        /* Shift matrix b up by one */ 
-        MPI_Sendrecv_replace(b, nlocal*nlocal, MPI_DOUBLE, 
-        uprank, 1, downrank, 1, comm_2d, &status); 
-
-//        if( gRank == 0 )
-//        {
-//            printMatrix(b,n,n);
-//            puts("");
-//        }
-    } 
-   
-      /* Restore the original distribution of a and b */ 
-//      MPI_Cart_shift(comm_2d, 0, +mycoords[0], &shiftsource, &shiftdest); 
-      MPI_Cart_shift(comm_2d, 1, +mycoords[0], &shiftsource, &shiftdest); 
-      MPI_Sendrecv_replace(a, nlocal*nlocal, MPI_DOUBLE, 
-          shiftdest, 1, shiftsource, 1, comm_2d, &status); 
-   
-//      MPI_Cart_shift(comm_2d, 1, +mycoords[1], &shiftsource, &shiftdest); 
-      MPI_Cart_shift(comm_2d, 0, +mycoords[1], &shiftsource, &shiftdest); 
-      MPI_Sendrecv_replace(b, nlocal*nlocal, MPI_DOUBLE, 
-          shiftdest, 1, shiftsource, 1, comm_2d, &status); 
-   
-      MPI_Comm_free(&comm_2d); /* Free up communicator */ 
-}
 
 
 
@@ -559,8 +417,10 @@ int main(int argc, char* argv[])
 
   printf("id=%d, before multiplication \n", id);
   //my_matmul(0, 0, 0, 0, 0, 0, n, n, n, n, a, b, c);
-  CannonAlgorithm(n, sa, sb, sc, comm);
+
   //MatrixMatrixMultiply_NonBlocking(n, sa, sb, sc, comm);
+
+  MatrixMultiply(n, sa, sb, sc);
   //MatrixMatrixMultiply(n, sa, sb, sc, comm);
   printf("id=%d, after multiplication \n", id);
 
@@ -574,11 +434,7 @@ int main(int argc, char* argv[])
 
   write_checkerboard_matrix(argv[3], (void**)c, mpitype, ma, ma, comm);
   
- // writeFile
 
- // if(id == 0){
-    //writeFile(sc, ma, ma);
- // }
 
   /* final timing */
   elapsed_time += MPI_Wtime();
