f551fe54af895293c76ad73127cb693a13387024
Jorge Travieso <jtrav029@fiu.edu>
2015-04-23 17:42:34 -0400

    saving still works after commenting sb2 and sa2 arrays

--- cannon/cannon.c
+++ cannon/cannon.c
@@ -29,7 +29,7 @@
 
 /* in case later we decide to use another data type */
 #define mpitype MPI_DOUBLE
-typedef double datatype;
+ typedef double datatype;
 
 /* block decomposition macros */
 #define BLOCK_LOW(id,p,n)  ((id)*(n)/(p))
@@ -38,8 +38,8 @@ typedef double datatype;
 #define BLOCK_OWNER(j,p,n) (((p)*((j)+1)-1)/(n))
 
 /* print out error message and exit the program */
-void my_abort(const char* fmt, ...)
-{
+ void my_abort(const char* fmt, ...)
+ {
   int id;     /* process rank */
   va_list ap; /* argument list */
 
@@ -54,7 +54,7 @@ void my_abort(const char* fmt, ...)
   /* all MPI processes exit at this point */
   exit(1);
 }
- 
+
 
 
 
@@ -63,13 +63,13 @@ void my_abort(const char* fmt, ...)
     /* This function performs a serial matrix-matrix multiplication c = a*b */ 
 void MatrixMultiply(int n, double *a, double *b, double *c) 
 { 
-    int i, j, k; 
+  int i, j, k; 
 
-    for (i=0; i<n; i++) 
-        for (j=0; j<n; j++) 
-            for (k=0; k<n; k++) 
-                c[i*n+j] += a[i*n+k]*b[k*n+j]; 
-}
+  for (i=0; i<n; i++) 
+    for (j=0; j<n; j++) 
+      for (k=0; k<n; k++) 
+        c[i*n+j] += a[i*n+k]*b[k*n+j]; 
+    }
 
 
 
@@ -77,35 +77,35 @@ void MatrixMultiply(int n, double *a, double *b, double *c)
 
 
 /* return the data size in bytes */
-int get_size(MPI_Datatype t)
-{
-   if(t == MPI_BYTE) return sizeof(char);
-   else if(t == MPI_DOUBLE) return sizeof(double);
-   else if(t == MPI_FLOAT) return sizeof(float);
-   else if(t == MPI_INT) return sizeof(int);
-   else {
-     printf ("Error: Unrecognized argument to 'get_size'\n");
-     fflush (stdout);
-     MPI_Abort (MPI_COMM_WORLD, -3);
+    int get_size(MPI_Datatype t)
+    {
+     if(t == MPI_BYTE) return sizeof(char);
+     else if(t == MPI_DOUBLE) return sizeof(double);
+     else if(t == MPI_FLOAT) return sizeof(float);
+     else if(t == MPI_INT) return sizeof(int);
+     else {
+       printf ("Error: Unrecognized argument to 'get_size'\n");
+       fflush (stdout);
+       MPI_Abort (MPI_COMM_WORLD, -3);
+     }
+     return 0;
    }
-   return 0;
-}
 
 
 /* allocate memory from heap */
-void *my_malloc(int id, int bytes)
-{
-   void *buffer;
-   if ((buffer = malloc ((size_t) bytes)) == NULL) {
+   void *my_malloc(int id, int bytes)
+   {
+     void *buffer;
+     if ((buffer = malloc ((size_t) bytes)) == NULL) {
       printf ("Error: Malloc failed for process %d\n", id);
       fflush (stdout);
       MPI_Abort (MPI_COMM_WORLD, -2);
-   }
-   return buffer;
-}
+    }
+    return buffer;
+  }
 
 /* Read a matrix from a file. */
-void read_checkerboard_matrix (
+  void read_checkerboard_matrix (
    char *s,              /* IN - File name */
    void ***subs,         /* OUT - 2D array */
    void **storage,       /* OUT - Array elements */
@@ -113,7 +113,7 @@ void read_checkerboard_matrix (
    int *m,               /* OUT - Array rows */
    int *n,               /* OUT - Array cols */
    MPI_Comm grid_comm)   /* IN - Communicator */
-{
+  {
    void      *buffer;         /* File buffer */
    int        coords[2];      /* Coords of proc receiving
                                  next row of matrix */
@@ -143,48 +143,48 @@ void read_checkerboard_matrix (
       number of cols, and broadcasts this information
       to the other processes. */
 
-   if (grid_id == 0) {
-      infileptr = fopen (s, "r");
-      if (infileptr == NULL) *m = 0;
-      else {
+      if (grid_id == 0) {
+        infileptr = fopen (s, "r");
+        if (infileptr == NULL) *m = 0;
+        else {
          fread (m, sizeof(int), 1, infileptr);
          fread (n, sizeof(int), 1, infileptr);
-      }
-   }
-   MPI_Bcast (m, 1, MPI_INT, 0, grid_comm);
+       }
+     }
+     MPI_Bcast (m, 1, MPI_INT, 0, grid_comm);
 
-   if (!(*m)) MPI_Abort (MPI_COMM_WORLD, -1);
+     if (!(*m)) MPI_Abort (MPI_COMM_WORLD, -1);
 
-   MPI_Bcast (n, 1, MPI_INT, 0, grid_comm);
+     MPI_Bcast (n, 1, MPI_INT, 0, grid_comm);
 
    /* Each process determines the size of the submatrix
       it is responsible for. */
 
-   MPI_Cart_get (grid_comm, 2, grid_size, grid_period,
+     MPI_Cart_get (grid_comm, 2, grid_size, grid_period,
       grid_coord);
-   local_rows = BLOCK_SIZE(grid_coord[0],grid_size[0],*m);
-   local_cols = BLOCK_SIZE(grid_coord[1],grid_size[1],*n);
+     local_rows = BLOCK_SIZE(grid_coord[0],grid_size[0],*m);
+     local_cols = BLOCK_SIZE(grid_coord[1],grid_size[1],*n);
 
    /* Dynamically allocate two-dimensional matrix 'subs' */
 
-   *storage = my_malloc (grid_id,
+     *storage = my_malloc (grid_id,
       local_rows * local_cols * datum_size);
-   *subs = (void **) my_malloc (grid_id,local_rows*sizeof(void*));
-   lptr = (void *) *subs;
-   rptr = (void *) *storage;
-   for (i = 0; i < local_rows; i++) {
+     *subs = (void **) my_malloc (grid_id,local_rows*sizeof(void*));
+     lptr = (void *) *subs;
+     rptr = (void *) *storage;
+     for (i = 0; i < local_rows; i++) {
       *(lptr++) = (void *) rptr;
       rptr += local_cols * datum_size;
-   }
+    }
 
    /* Grid process 0 reads in the matrix one row at a time
       and distributes each row among the MPI processes. */
 
-   if (grid_id == 0)
+    if (grid_id == 0)
       buffer = my_malloc (grid_id, *n * datum_size);
 
    /* For each row of processes in the process grid... */
-   for (i = 0; i < grid_size[0]; i++) {
+    for (i = 0; i < grid_size[0]; i++) {
       coords[0] = i;
 
       /* For each matrix row controlled by this proc row...*/
@@ -192,62 +192,62 @@ void read_checkerboard_matrix (
 
          /* Read in a row of the matrix */
 
-         if (grid_id == 0) {
-            fread (buffer, datum_size, *n, infileptr);
-         }
+       if (grid_id == 0) {
+        fread (buffer, datum_size, *n, infileptr);
+      }
 
          /* Distribute it among process in the grid row */
 
-         for (k = 0; k < grid_size[1]; k++) {
-            coords[1] = k;
+      for (k = 0; k < grid_size[1]; k++) {
+        coords[1] = k;
 
             /* Find address of first element to send */
-            raddr = buffer +
-               BLOCK_LOW(k,grid_size[1],*n) * datum_size;
+        raddr = buffer +
+        BLOCK_LOW(k,grid_size[1],*n) * datum_size;
 
             /* Determine the grid ID of the process getting
                the subrow */
-            MPI_Cart_rank (grid_comm, coords, &dest_id);
+        MPI_Cart_rank (grid_comm, coords, &dest_id);
 
             /* Process 0 is responsible for sending...*/
-            if (grid_id == 0) {
+        if (grid_id == 0) {
 
                /* It is sending (copying) to itself */
-               if (dest_id == 0) {
-                  laddr = (*subs)[j];
-                  memcpy (laddr, raddr,
-                     local_cols * datum_size);
+         if (dest_id == 0) {
+          laddr = (*subs)[j];
+          memcpy (laddr, raddr,
+           local_cols * datum_size);
 
                /* It is sending to another process */
-               } else {
-                  MPI_Send (raddr,
-                     BLOCK_SIZE(k,grid_size[1],*n), dtype,
-                  dest_id, 0, grid_comm);
-               }
+        } else {
+          MPI_Send (raddr,
+           BLOCK_SIZE(k,grid_size[1],*n), dtype,
+           dest_id, 0, grid_comm);
+        }
 
             /* Process 'dest_id' is responsible for
                receiving... */
-            } else if (grid_id == dest_id) {
-               MPI_Recv ((*subs)[j], local_cols, dtype, 0,
-                  0, grid_comm,&status);
-            }
-         }
-      }
+      } else if (grid_id == dest_id) {
+       MPI_Recv ((*subs)[j], local_cols, dtype, 0,
+        0, grid_comm,&status);
+     }
    }
-   if (grid_id == 0) free (buffer);
+ }
+}
+if (grid_id == 0) free (buffer);
 }
 
 /*
  * Write a matrix distributed in checkerboard fashion to a file.
  */
-void write_checkerboard_matrix (
+ void write_checkerboard_matrix (
    char        *s,	      /* IN -File name */
    void       **a,            /* IN -2D matrix */
    MPI_Datatype dtype,        /* IN -Matrix element type */
    int          m,            /* IN -Matrix rows */
    int          n,            /* IN -Matrix columns */
    MPI_Comm     grid_comm)    /* IN -Communicator */
-{
+ {
    void      *buffer;         /* Room to hold 1 matrix row */
    int        coords[2];      /* Grid coords of process
                                  sending elements */
@@ -272,54 +272,54 @@ void write_checkerboard_matrix (
    if(grid_id == 0) {
      outfileptr = fopen (s, "w");
      if(!outfileptr ||
-	fwrite(&m, sizeof(int), 1, outfileptr) != 1 ||
-	fwrite(&n, sizeof(int), 1, outfileptr) != 1)
+       fwrite(&m, sizeof(int), 1, outfileptr) != 1 ||
+       fwrite(&n, sizeof(int), 1, outfileptr) != 1)
        MPI_Abort (MPI_COMM_WORLD, -1);
    }
 
    MPI_Cart_get (grid_comm, 2, grid_size, grid_period,
-      grid_coords);
+    grid_coords);
    local_cols = BLOCK_SIZE(grid_coords[1],grid_size[1],n);
 
    if (!grid_id)
-      buffer = my_malloc (grid_id, n*datum_size);
+    buffer = my_malloc (grid_id, n*datum_size);
 
    /* For each row of the process grid */
-   for (i = 0; i < grid_size[0]; i++) {
-      coords[0] = i;
+  for (i = 0; i < grid_size[0]; i++) {
+    coords[0] = i;
 
       /* For each matrix row controlled by the process row */
-      for (j = 0; j < BLOCK_SIZE(i,grid_size[0],m); j++) {
+    for (j = 0; j < BLOCK_SIZE(i,grid_size[0],m); j++) {
 
          /* Collect the matrix row on grid process 0 and
             print it */
-         if (!grid_id) {
-            for (k = 0; k < grid_size[1]; k++) {
-               coords[1] = k;
-               MPI_Cart_rank (grid_comm, coords, &src);
-               els = BLOCK_SIZE(k,grid_size[1],n);
-               laddr = buffer +
-                  BLOCK_LOW(k,grid_size[1],n) * datum_size;
-               if (src == 0) {
-                  memcpy (laddr, a[j], els * datum_size);
-               } else {
-                  MPI_Recv(laddr, els, dtype, src, 0,
-                     grid_comm, &status);
-               }
-            }
-
-	    if(fwrite(buffer, datum_size, n, outfileptr) != n)
-	      MPI_Abort (MPI_COMM_WORLD, -1);
-         } else if (grid_coords[0] == i) {
-            MPI_Send (a[j], local_cols, dtype, 0, 0,
-               grid_comm);
-         }
+     if (!grid_id) {
+      for (k = 0; k < grid_size[1]; k++) {
+       coords[1] = k;
+       MPI_Cart_rank (grid_comm, coords, &src);
+       els = BLOCK_SIZE(k,grid_size[1],n);
+       laddr = buffer +
+       BLOCK_LOW(k,grid_size[1],n) * datum_size;
+       if (src == 0) {
+        memcpy (laddr, a[j], els * datum_size);
+      } else {
+        MPI_Recv(laddr, els, dtype, src, 0,
+         grid_comm, &status);
       }
-   }
-   if (!grid_id) {
-      free (buffer);
-      fclose(outfileptr);
-   }
+    }
+
+    if(fwrite(buffer, datum_size, n, outfileptr) != n)
+     MPI_Abort (MPI_COMM_WORLD, -1);
+ } else if (grid_coords[0] == i) {
+  MPI_Send (a[j], local_cols, dtype, 0, 0,
+   grid_comm);
+}
+}
+}
+if (!grid_id) {
+  free (buffer);
+  fclose(outfileptr);
+}
 }
 
 /* recursive, block-oriented, sequential matrix multiplication */
@@ -329,37 +329,37 @@ void my_matmul(int crow, int ccol, /* corner of C block */
 	       int l, int m, int n, /* block A is l*m, block B is m*n, block C is l*n */
 	       int N, /* matrices are N*N */
 	       datatype** a, datatype** b, datatype** c) { /* 2D matrices */
-  int i, j, k; 
+int i, j, k; 
   int lhalf[3], mhalf[3], nhalf[3]; /* quadrant sizes */
-  datatype *aptr, *bptr, *cptr;
+datatype *aptr, *bptr, *cptr;
 
   if(m*n*sizeof(datatype) > CACHE_SIZE) { /* block B doesn't fit in cache */
-    lhalf[0] = 0; lhalf[1] = l/2; lhalf[2] = l-l/2;
-    mhalf[0] = 0; mhalf[1] = m/2; mhalf[2] = m-m/2;
-    nhalf[0] = 0; nhalf[1] = n/2; nhalf[2] = n-n/2;
-    for(i=0; i<2; i++)
-      for(j=0; j<2; j++)
-	for(k=0; k<2; k++)
-	  my_matmul(crow+lhalf[i], ccol+nhalf[j],
-		    arow+lhalf[i], acol+mhalf[k],
-		    brow+mhalf[k], bcol+nhalf[j],
-		    lhalf[i+1], mhalf[k+1], nhalf[j+1],
-		    N, a, b, c);
+lhalf[0] = 0; lhalf[1] = l/2; lhalf[2] = l-l/2;
+mhalf[0] = 0; mhalf[1] = m/2; mhalf[2] = m-m/2;
+nhalf[0] = 0; nhalf[1] = n/2; nhalf[2] = n-n/2;
+for(i=0; i<2; i++)
+  for(j=0; j<2; j++)
+   for(k=0; k<2; k++)
+     my_matmul(crow+lhalf[i], ccol+nhalf[j],
+      arow+lhalf[i], acol+mhalf[k],
+      brow+mhalf[k], bcol+nhalf[j],
+      lhalf[i+1], mhalf[k+1], nhalf[j+1],
+      N, a, b, c);
   } else { /* block B fits in cache */
-    for(i=0; i<l; i++) {
-      for(j=0; j<n; j++) {
-	cptr = &c[crow+i][ccol+j];
-	aptr = &a[arow+i][acol];
-	bptr = &b[brow][bcol+j];
-	for(k=0; k<m; k++) {
-	  *cptr += *(aptr++) * (*bptr);
-	  bptr += N;
-	}
-      }
-    }
-  }
+   for(i=0; i<l; i++) {
+    for(j=0; j<n; j++) {
+     cptr = &c[crow+i][ccol+j];
+     aptr = &a[arow+i][acol];
+     bptr = &b[brow][bcol+j];
+     for(k=0; k<m; k++) {
+       *cptr += *(aptr++) * (*bptr);
+       bptr += N;
+     }
+   }
+ }
 }
-	       
+}
+
 /*
 void cannon(int n, datatype ** a, datatype ** b, datatype ** c, datatype * sa ,datatype * sb, datatype * sc, MPI_Comm, comm){
 
@@ -431,15 +431,15 @@ int main(int argc, char* argv[])
   c = (datatype**)malloc(n*sizeof(datatype*));
   for(i=0; i<n; i++) c[i] = &sc[i*n];
 
-  printf("id=%d, before multiplication \n", id);
+    printf("id=%d, before multiplication \n", id);
   //my_matmul(0, 0, 0, 0, 0, 0, n, n, n, n, a, b, c);
 
 
 
 
- 
+
  //*****************************************************
- 
+
   int shiftsource, shiftdest;
   int uprank, downrank, leftrank, rightrank;
 
@@ -448,75 +448,74 @@ int main(int argc, char* argv[])
   MPI_Cart_shift(comm, 0, -1, &downrank, &uprank); 
 
      /* Perform the initial matrix alignment. First for A and then for B */ 
-     MPI_Cart_shift(comm, 1, -coord[0], &shiftsource, &shiftdest); 
-     MPI_Sendrecv_replace(a[0], n*n, MPI_DOUBLE, 
-         shiftdest, 1, shiftsource, 1, comm, &status); 
- 
-     MPI_Cart_shift(comm, 0, -coord[1], &shiftsource, &shiftdest); 
-     MPI_Sendrecv_replace(b[0], n*n, MPI_DOUBLE, 
-         shiftdest, 1, shiftsource, 1, comm, &status); 
+  MPI_Cart_shift(comm, 1, -coord[0], &shiftsource, &shiftdest); 
+  MPI_Sendrecv_replace(a[0], n*n, MPI_DOUBLE, 
+   shiftdest, 1, shiftsource, 1, comm, &status); 
+
+  MPI_Cart_shift(comm, 0, -coord[1], &shiftsource, &shiftdest); 
+  MPI_Sendrecv_replace(b[0], n*n, MPI_DOUBLE, 
+   shiftdest, 1, shiftsource, 1, comm, &status); 
 
 
 
 
 
    /* Setup the a_buffers and b_buffers arrays */ 
- datatype **a_buffer[2], **b_buffer[2];
- datatype *sa2, *sb2;
- datatype **a2, **b2;
- a_buffer[0] = a;
- sa2 = (datatype*)malloc(n*n*sizeof(datatype));
- memset(sa2, 0, n*n*sizeof(datatype));
- a2 = (datatype**)malloc(n*sizeof(datatype*));
- for(i=0; i<n; i++) a2[i] = &sa2[i*n];
- a_buffer[1] = a2;
+  datatype **a_buffer[2], **b_buffer[2];
+  datatype **a2, **b2;
+  a_buffer[0] = a;
+  sa = (datatype*)malloc(n*n*sizeof(datatype));
+  memset(sa, 0, n*n*sizeof(datatype));
+  a2 = (datatype**)malloc(n*sizeof(datatype*));
+  for(i=0; i<n; i++) a2[i] = &sa[i*n];
+  a_buffer[1] = a2;
 
  b_buffer[0] = b;
- sb2 = (datatype*)malloc(n*n*sizeof(datatype));
- memset(sb2, 0, n*n*sizeof(datatype));
+ sb = (datatype*)malloc(n*n*sizeof(datatype));
+ memset(sb, 0, n*n*sizeof(datatype));
  b2 = (datatype**)malloc(n*sizeof(datatype*));
- for(i=0; i<n; i++) b2[i] = &sb2[i*n];
- b_buffer[1] = b2;
+ for(i=0; i<n; i++) b2[i] = &sb[i*n];
+   b_buffer[1] = b2;
 
 
  /* Get into the main computation loop */ 
  for (i = 0 ; i < dim[0] ; i++) 
  { 
-     MPI_Isend(a_buffer[i%2][0], n*n, MPI_DOUBLE, 
-          leftrank, 1, comm, &reqs[0]); 
-      MPI_Isend(b_buffer[i%2][0], n*n, MPI_DOUBLE, 
-          uprank, 1, comm, &reqs[1]); 
-      MPI_Irecv(a_buffer[(i+1)%2][0], n*n, MPI_DOUBLE, 
-          rightrank, 1, comm, &reqs[2]); 
-      MPI_Irecv(b_buffer[(i+1)%2][0], n*n, MPI_DOUBLE, 
-          downrank, 1, comm, &reqs[3]);
-      my_matmul(0, 0, 0, 0, 0, 0, n, n, n, n, a_buffer[i%2], b_buffer[i%2], c);
+   MPI_Isend(a_buffer[i%2][0], n*n, MPI_DOUBLE, 
+    leftrank, 1, comm, &reqs[0]); 
+   MPI_Isend(b_buffer[i%2][0], n*n, MPI_DOUBLE, 
+    uprank, 1, comm, &reqs[1]); 
+   MPI_Irecv(a_buffer[(i+1)%2][0], n*n, MPI_DOUBLE, 
+    rightrank, 1, comm, &reqs[2]); 
+   MPI_Irecv(b_buffer[(i+1)%2][0], n*n, MPI_DOUBLE, 
+    downrank, 1, comm, &reqs[3]);
+   my_matmul(0, 0, 0, 0, 0, 0, n, n, n, n, a_buffer[i%2], b_buffer[i%2], c);
 
    for (j=0; j<4; j++) 
-        MPI_Wait(&reqs[j], &status);
- }
+    MPI_Wait(&reqs[j], &status);
+}
 
 
 
 
 
   //MatrixMatrixMultiply(n, sa, sb, sc, comm);
-  printf("id=%d, after multiplication \n", id);
+printf("id=%d, after multiplication \n", id);
 
   //testing
   //for(i=0; i<n; i++) c[i] = &sc[i*n];
 
   /* write the submatrix of C managed by this process */
-  write_checkerboard_matrix(argv[3], (void**)c, mpitype, ma, ma, comm);
-  
+write_checkerboard_matrix(argv[3], (void**)c, mpitype, ma, ma, comm);
+
 
 
   /* final timing */
-  elapsed_time += MPI_Wtime();
-  if(!id) printf("elapsed time: %lf\n", elapsed_time);
-  
-  MPI_Finalize();
-  return 0;
+elapsed_time += MPI_Wtime();
+if(!id) printf("elapsed time: %lf\n", elapsed_time);
+
+MPI_Finalize();
+return 0;
 }
 
 
